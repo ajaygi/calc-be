9d9ab89c57e718a9849e5859cfcd024212e9fadb
Ajay Gupta <javadev.ajay@gmail.com>
2022-06-21 08:23:59 +0530

    SubService slf4j annotation and logger added

--- src/main/java/com/redhat/openinnovationlabs/microservice/calculator/service/SubService.java
+++ src/main/java/com/redhat/openinnovationlabs/microservice/calculator/service/SubService.java
@@ -2,14 +2,17 @@ package com.redhat.openinnovationlabs.microservice.calculator.service;
 
 import com.redhat.openinnovationlabs.microservice.calculator.model.ApiResult;
 import com.redhat.openinnovationlabs.microservice.calculator.model.ResultBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @Service
 public class SubService {
 
     public ApiResult subCalc(BigDecimal operand_1, BigDecimal operand_2){
+//        log.info("SubService subCalc method called....");
         return ResultBuilder.getResult(operand_1.subtract(operand_2));
     }
 }
