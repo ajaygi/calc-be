6455cfdcf041dd92398e2b6d721fd61bf5121746
Ajay Gupta <javadev.ajay@gmail.com>
2022-06-21 08:19:33 +0530

    lombok dependency added

--- src/main/java/com/redhat/openinnovationlabs/microservice/calculator/basic/DivController.java
+++ src/main/java/com/redhat/openinnovationlabs/microservice/calculator/basic/DivController.java
@@ -3,6 +3,7 @@ package com.redhat.openinnovationlabs.microservice.calculator.basic;
 import com.redhat.openinnovationlabs.microservice.calculator.model.ApiResult;
 import com.redhat.openinnovationlabs.microservice.calculator.model.ResultBuilder;
 import com.redhat.openinnovationlabs.microservice.calculator.service.DivService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @RestController
 @Profile("div")
 public class DivController {
